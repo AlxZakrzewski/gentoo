e330201746784e82bc87c6abebc5990f7a6d170e
babro <babro@babro.com>
2023-01-20 17:17:43 +0100

    Update

---
+++ asus/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget http://gentoo.mirror.web4u.cz/releases/amd64/autobuilds/current-stage3-amd64-hardened-openrc/stage3-amd64-desktop-openrc-20230115T170214Z.tar.xz
+tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run

064896680b8873f2fe827da72e40fe3141abf9bc
babro_gentoo <babro@babro.com>
2023-01-22 18:52:13 +0100

    add grub package

--- asus/packages_base.sh
+++ asus/packages_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz4 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim
+emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz4 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim sys-boot/grub
 
 rc-update add dhcpcd default
 rc-service dhcpcd start

39b4f3c8635643068d5bc512b3d5d7afd9853d0e
babro_gentoo <babro@babro.com>
2023-01-22 19:40:46 +0100

    change grub version

--- asus/packages_base.sh
+++ asus/packages_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz4 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim sys-boot/grub
+emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz4 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim sys-boot/grub:2
 
 rc-update add dhcpcd default
 rc-service dhcpcd start

45bfba7c9fb6e587aa25a8f3e6f1f99f092c95a5
babro_gentoo <babro@babro.com>
2023-01-22 21:20:46 +0100

    update

--- asus/packages_base.sh
+++ asus/packages_base.sh
@@ -3,6 +3,6 @@ set -e
 emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz4 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim sys-boot/grub:2
 
 rc-update add dhcpcd default
-rc-service dhcpcd start
 rc-update add sysklogd default
 rc-update add chronyd default
+rc-service dhcpcd start

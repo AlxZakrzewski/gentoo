0ec0efe213e66816f9e42e8ed700ab57c74ecf2c
babro <babro@gentoo.com>
2023-02-10 17:40:12 +0100

    add clipmenu

--- dwm/install.sh
+++ dwm/install.sh
@@ -9,7 +9,11 @@ sed -i s/Mod1Mask/Mod4Mask/g $F
 sed -i 's/{ "1", "2", "3", "4", "5", "6", "7", "8", "9" }/{ "1", "2", "3" }/g' $F
 sed -i 's/borderpx  = 1/borderpx  = 0/g' $F
 sed -i 's/col_cyan\[\]        = "#005577"/col_cyan\[\]        = "#24445c"/g' $F
-sed -i 's/{ "St"/{ "st-256color/g' $F
+sed -i 's/{ "St"/{ "st-256color"/g' $F
+sed -i '/class     instance  title           tags mask  isfloating/a \    { \"clipmenu\",     NULL,       NULL,       1 \<\< 0,       False,       -1 \},' $F
+sed -i '/key        function        argument/a\    \{ MODKEY,                       XK_o,      spawn,          \{\.v = clipmenumd \} \},' $F
+sed -i '/\/\* commands \*\//a\static const char  *clipmenumd[] =
+{ "clipmenu",  NULL };\' $F
 
 make
 

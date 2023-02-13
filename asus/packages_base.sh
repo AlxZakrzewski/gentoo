deacfcfbdbd68550e3d4088b3bfdc9826e5dd486
babro <babro@gentoo.com>
2023-02-13 20:57:40 +0100

    add cups

--- asus/packages_base.sh
+++ asus/packages_base.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 echo $'dev-libs/go-fuse **\nsys-fs/go-mtpfs **' > /etc/portage/package.accept_keywords
-emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz5 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim sys-boot/grub:2 dev-vcs/git sys-apps/usbutils app-admin/sudo app-eselect/eselect-repository x11-base/xorg-drivers x11-base/xorg-server x11-drivers/nvidia-drivers x11-terms/st x11-wm/dwm sys-process/htop media-sound/audacity media-sound/pulseaudio media-sound/pavucontrol media-plugins/alsa-plugins app-admin/keepassxc app-emulation/wine-vanilla app-misc/solaar sys-fs/ntfs4g media-gfx/feh sys-fs/fuse sys-fs/go-mtpfs app-text/tree net-misc/yt-dlp sys-fs/go-mtpfs x11-apps/xsetroot x11-misc/clipmenu app-misc/piper
+emerge -q sys-kernel/gentoo-sources app-arch/lzop app-arch/lz5 sys-apps/pciutils net-misc/dhcpcd app-admin/sysklogd net-misc/chrony sys-fs/dosfstools net-wireless/iw net-wireless/wpa_supplicant app-editors/vim sys-boot/grub:2 dev-vcs/git sys-apps/usbutils app-admin/sudo app-eselect/eselect-repository x11-base/xorg-drivers x11-base/xorg-server x11-drivers/nvidia-drivers x11-terms/st x11-wm/dwm sys-process/htop media-sound/audacity media-sound/pulseaudio media-sound/pavucontrol media-plugins/alsa-plugins app-admin/keepassxc app-emulation/wine-vanilla app-misc/solaar sys-fs/ntfs4g media-gfx/feh sys-fs/fuse sys-fs/go-mtpfs app-text/tree net-misc/yt-dlp sys-fs/go-mtpfs x11-apps/xsetroot x11-misc/clipmenu app-misc/piper net-print/cups
 
 rc-update add dhcpcd default
 rc-update add sysklogd default
 rc-update add chronyd default
+rc-update add cupsd default
 rc-service dhcpcd start
+rc-service cupsd start
 # install brave
 eselect repository enable brave-overlay
 emaint sync -r brave-overlay

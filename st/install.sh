c4acc9fab4ea1268901b88af31a944b0aa18d7bc
a.zakrzewski <a.zakrzewski@gentoo.com>
2024-02-06 12:53:02 +0100

    st added

--- st/install.sh
+++ st/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 patch -p1 < patches/st-scrollback-ringbuffer-0.8.5.diff
+patch -p1 < patches/st-scrollback-reflow-0.9.diff
+patch -p1 < patches/st-scrollback-mouse-20170427-5a10aca.diff
+patch -p1 < patches/st-scrollback-mouse-altscreen-20170427-5a10aca.diff
 patch -p1 < patches/st-alpha-20220206-0.8.5.diff
 
 make

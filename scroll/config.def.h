ef59352fc14ef15c9e54bf19fdd2b6242c61db5a
a.zakrzewski <a.zakrzewski@gentoo.com>
2024-02-06 13:49:53 +0100

    st added

--- scroll/config.def.h
+++ scroll/config.def.h
@@ -10,6 +10,6 @@ struct rule rules[] = {
 	/* sequence     event        lines */
 	{"\033[5;2~",   SCROLL_UP,   -1},       /* [Shift] + [PageUP] */
 	{"\033[6;2~",   SCROLL_DOWN, -1},       /* [Shift] + [PageDown] */
-	{"\031",        SCROLL_UP,    1},       /* mouse wheel up */
-	{"\033[1;6B",        SCROLL_DOWN,  1},       /* mouse wheel Down */
+	{"^[[1;5A",        SCROLL_UP,    1},       
+	{"^[[1;5B",        SCROLL_DOWN,  1},       
 };

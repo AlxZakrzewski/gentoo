c5d49d2fc766397404deaa5b9c5aaebdaa175d55
a.zakrzewski <a.zakrzewski@gentoo.com>
2024-02-07 21:18:11 +0100

    dwm patch colors

--- dwm/install.sh
+++ dwm/install.sh
@@ -3,7 +3,7 @@
 #patch -p1 < patches/dwm-actualfullscreen-20211013-cb3f58a.patch
 #patch -p1 < patches/dwm-fullgaps-20200508-7b77734.diff
 #patch -p1 < patches/dwm-swallow-6.3.diff
-patch -p1 < patches/dwm-xrdb-6.4.diff
+patch -p1 < patches/dwm-colorbar-6.2.diff
 
 F=config.def.h
 sed -i s/Mod1Mask/Mod4Mask/g $F

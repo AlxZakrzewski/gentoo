b5971ccf6ad9fa9644346cf330d2ef6d3d1dcf3f
a.zakrzewski <a.zakrzewski@gentoo.com>
2024-02-08 18:03:21 +0100

    fix dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#9A5213";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
--- dwm/install.sh
+++ dwm/install.sh
@@ -2,8 +2,6 @@
 
 patch -p1 < patches/dwm-actualfullscreen-20211013-cb3f58a.patch
 patch -p1 < patches/dwm-fullgaps-20200508-7b77734.diff
-patch -p1 < patches/dwm-colorbar-6.2.diff
-patch -p1 < patches/dwm-swallow-6.3.diff
 
 F=config.def.h
 sed -i s/Mod1Mask/Mod4Mask/g $F

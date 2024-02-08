98e86ad54ea4e31ce603221f05abe429b97e80d2
a.zakrzewski <a.zakrzewski@gentoo.com>
2024-02-08 18:05:12 +0100

    fix dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -9,7 +9,7 @@ static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#9A5213";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#4E2F0A";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {

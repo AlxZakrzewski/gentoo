3c223b7d9bd0fadd893b53f0f8921f788457ca89
a.zakrzewski <a.zakrzewski@gentoo.com>
2024-02-08 20:55:26 +0100

    fix dwm

--- dwm/install.sh
+++ dwm/install.sh
@@ -2,7 +2,6 @@
 
 patch -p1 < patches/dwm-actualfullscreen-20211013-cb3f58a.patch
 patch -p1 < patches/dwm-fullgaps-20200508-7b77734.diff
-patch -p1 < patches/dwm-alpha-20230401-348f655.diff
 
 F=config.def.h
 
